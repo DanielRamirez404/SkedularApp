089fb1ef424626e5141c035157577b84908470e6
DanielRamirez404 <danielramirezabou404@gmail.com>
2025-11-11 00:50:05 -0400

    Add the material icons dependency to add a more options icon to the HomewordCard component's right side

--- app/src/main/java/com/example/skedularapp/components/Home.kt
+++ app/src/main/java/com/example/skedularapp/components/Home.kt
@@ -11,8 +11,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.materialIcon
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -55,19 +60,32 @@ fun HomeworkCard(
         )
     ) {
         Row(
-            modifier = Modifier.padding(16.dp),
-            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Box(
-                modifier = Modifier
-                    .size(35.dp)
-                    .background(color, CircleShape)
-            )
-            Column {
-                HomeworkCardText(text = title, alpha = 0.8f, style = MaterialTheme.typography.titleMedium)
-                HomeworkCardText(text = "$subject • $dueTime", alpha = 0.6f, style = MaterialTheme.typography.bodySmall)
+            Row(
+                modifier = Modifier.padding(16.dp),
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size(35.dp)
+                        .background(color, CircleShape)
+                )
+                Column {
+                    HomeworkCardText(text = title, alpha = 0.8f, style = MaterialTheme.typography.titleMedium)
+                    HomeworkCardText(text = "$subject • $dueTime", alpha = 0.6f, style = MaterialTheme.typography.bodySmall)
+                }
             }
+            Icon(
+                modifier = Modifier.padding(end = 16.dp),
+                imageVector = Icons.Filled.MoreVert,
+                contentDescription = "Settings",
+                tint = MaterialTheme.colorScheme.onPrimaryContainer.copy(alpha = 0.6f)
+            )
         }
     }
 }
