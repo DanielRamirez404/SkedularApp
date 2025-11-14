9432add23d7a4defa9104d09fe581ce2ecea7004
DanielRamirez404 <danielramirezabou404@gmail.com>
2025-11-13 22:47:49 -0400

    Style the SegmentedButtonWeek component

--- app/src/main/java/com/example/skedularapp/components/Home.kt
+++ app/src/main/java/com/example/skedularapp/components/Home.kt
@@ -8,10 +8,15 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.materialIcon
@@ -31,9 +36,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.skedularapp.utilities.getDayOfDate
 import com.example.skedularapp.utilities.getMondayOfWeek
 import com.example.skedularapp.utilities.getNextWeek
@@ -105,6 +112,16 @@ fun HomeworkCard(
 }
 
 @Composable
+fun DateIcon(modifier: Modifier = Modifier) {
+    Icon(
+        modifier = modifier.offset(y = (-1).dp),
+        imageVector = Icons.Filled.DateRange,
+        contentDescription = "Date",
+        tint = MaterialTheme.colorScheme.onPrimaryContainer.copy(alpha = 0.6f)
+    )
+}
+
+@Composable
 fun SegmentedButtonWeek(modifier: Modifier = Modifier, date: Date) {
     val dayOfWeekNumber = getDayOfWeekNumber(date)
     var selectedIndex by remember { mutableIntStateOf(dayOfWeekNumber - 1) }
@@ -114,20 +131,44 @@ fun SegmentedButtonWeek(modifier: Modifier = Modifier, date: Date) {
 
     val options = listOf("Lun", "Mar", "Mie", "Jue", "Vie")
 
-    SingleChoiceSegmentedButtonRow {
+    SingleChoiceSegmentedButtonRow(modifier = modifier) {
         options.forEachIndexed { index, label ->
 
             val optionDate = getNthDayAfter(monday, index)
             val dayNumber = getDayOfDate(optionDate)
 
+            val selected = index == selectedIndex
+
             SegmentedButton(
-                shape = SegmentedButtonDefaults.itemShape(
-                    index = index,
-                    count = options.size
-                ),
+                modifier = modifier
+                    .padding(horizontal = 5.dp)
+                    .height(33.dp),
+
+                border = BorderStroke(1.dp, MaterialTheme.colorScheme.outlineVariant),
+                shape = RoundedCornerShape(5.dp),
+
                 onClick = { selectedIndex = index },
-                selected = index == selectedIndex,
-                label = { Text("$label $dayNumber") }
+                selected = selected,
+                icon = {
+                    SegmentedButtonDefaults.Icon(
+                        active = selected,
+                        activeContent = { DateIcon(modifier = modifier.padding(start = 8.dp)) },
+                    )
+                },
+
+                label = {
+                    Text(
+                        modifier = Modifier.padding(end = if (selected) 2.dp else 0.dp),
+                        text = "$label $dayNumber",
+                        style = MaterialTheme.typography.bodySmall,
+                        fontWeight = if (selected) FontWeight.Medium else FontWeight.Normal,
+                    )
+                },
+
+                colors = SegmentedButtonDefaults.colors(
+                    activeContainerColor = MaterialTheme.colorScheme.primaryContainer.copy(alpha = 0.75f),
+                    activeContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                )
             )
         }
     }
